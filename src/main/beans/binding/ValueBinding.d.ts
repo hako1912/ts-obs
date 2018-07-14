76180d169634dd6145381d2ee3f067eb23c427d9
koya <koyakon.dev@gmail.com>
2018-07-14 14:54:18 +0900

    clearn

--- src/main/beans/binding/ValueBinding.d.ts
+++
@@ -1,7 +0,0 @@
-import ObsValue from "@/beans/ObsValue";
-export default class ValueBinding<T, U = T> {
-    private obs;
-    private mapper;
-    constructor(obs: ObsValue<T>, mapper: (val: T) => U);
-    private val;
-}
--- src/main/funciton/funciton.d.ts
+++
@@ -1,4 +0,0 @@
-export declare module Functions {
-    function curry<T, U, R>(func: (a1: T, a2: U) => R): (v1: T) => (v2: U) => R;
-    function part<T, U, R>(func: (a1: T, a2: U) => R, arg: T): (v2: U) => R;
-}
