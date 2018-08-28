b9ace7752eedec8774bdb1e275dcc5b5ced6f11a
koya <[hatakoya1@gmail.com]>
2018-08-29 00:59:57 +0900

    repository

--- src/main/domain/Repository.ts
+++ src/main/domain/Repository.ts
@@ -4,7 +4,7 @@ import Entity from "./Entity";
 export default abstract class Repository<E extends Entity> {
 
     private incrementalId = 0
-    
+
     // ホントはprotectedにしたいけどLeftJoinedListを作るときにこまる
     public store = new IndexedList<number, E>(it => it.$id)
 
@@ -20,35 +20,40 @@ export default abstract class Repository<E extends Entity> {
         return this.store.values
     }
 
-    insert(entity: E): number {
-        // 連番キーを割り当てる
-        if (entity.$id == null || entity.$id < 0) {
-            entity.$id = this.incrementalId++
-        }
-        // すでに存在するキーに対して挿入しようとした場合
-        if (this.has(entity.$id)) {
-            throw Error(`key=${entity.$id} is already exists.`)
-        }
-        this.store.push(entity)
-        return entity.$id
+    insert(...entities: E[]): void {
+        entities.forEach(entity => {
+            // 連番キーを割り当てる
+            if (entity.$id == null || entity.$id < 0) {
+                entity.$id = this.incrementalId++
+            }
+            // すでに存在するキーに対して挿入しようとした場合
+            if (this.has(entity.$id)) {
+                throw Error(`key=${entity.$id} is already exists.`)
+            }
+        })
+        this.store.push(...entities)
     }
 
-    update(entity: E) {
-        if (entity.$id == null || entity.$id < 0) {
-            throw Error(`updateError: key is not assigned.`)
-        }
-        const updateTarget = this.store.val.find(it => it.value.$id === entity.$id)
-        if (updateTarget == null) {
-            throw Error(`updateError: key=${entity.$id} is not exists.`)
-        }
-        updateTarget.value = entity
+    update(...entities: E[]) {
+        entities.forEach(entity => {
+            if (entity.$id == null || entity.$id < 0) {
+                throw Error(`updateError: key is not assigned.`)
+            }
+            const updateTarget = this.store.val.find(it => it.value.$id === entity.$id)
+            if (updateTarget == null) {
+                throw Error(`updateError: key=${entity.$id} is not exists.`)
+            }
+            updateTarget.value = entity
+        })
     }
 
-    deleteBy(id: number): void {
-        if (!this.has(id)) {
-            throw Error(`deleteError: key=${id} is not exists.`)
-        }
-        this.store.removeIf(it => it.$id === id)
+    deleteBy(...ids: number[]): void {
+        ids.forEach(id => {
+            if (!this.has(id)) {
+                throw Error(`deleteError: key=${id} is not exists.`)
+            }
+            this.store.removeIf(it => it.$id === id)
+        })
     }
 
     truncate() {
